77e9875c07523665b76f2c02e137f764495e8dc7
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-01-20 03:32:22 +0100

    before esense madness

--- mobile/lib/routes/game/controls.dart
+++ mobile/lib/routes/game/controls.dart
@@ -1,3 +1,5 @@
+import 'package:mobile/external/protocol/interfaces/game.dart';
+
 enum ControlTypes {
   eSense,
   // swipe,
@@ -7,5 +9,30 @@ enum ControlTypes {
 extension ParseToValue on ControlTypes {
   String toValue() {
     return toString().split('.').last.toUpperCase();
+  }
+}
+
+abstract class GameControls {
+  // same reasoning with <insert filename here>
+  // ignore: prefer_function_declarations_over_variables
+  void Function(Direction) dirCb = (_) {};
+
+  onDirection(Function(Direction) cb) {
+    dirCb = cb;
+  }
+
+  destroy() {}
+
+  static get(ControlTypes c) {
+    switch (c) {
+      case ControlTypes.eSense: return ESense();
+    }
+  }
+}
+
+class ESense extends GameControls {
+  late String eSenseName;
+  init(String name) {
+    eSenseName = name;
   }
 }
